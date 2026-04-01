e8d0fe685088005499a3c44111b5193d901236f7
Thiago04 <thiagocecon@unochapeco.edu.br>
2026-04-01 20:42:05 -0300

    exercicios01-04

---
+++ aula0104.js
@@ -0,0 +1,31 @@
+for (let i = 10; i > 0; i--) {
+    console.log("Contagem: " + i);
+}
+
+let i = 10;
+while (i > 10) {   
+    console.log("Contagem: " - i);
+    i--;
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log("5 * " + i + " = " + (5 * i));
+}
+ 
+let i = 5;
+while (i <= 50) {
+    console.log(i);
+    i += 5;
+}
+
+let i = 10;
+do {
+    console.log("Contagem: " + i);
+    i--;
+}while (i > 0);
+
+let i = 5;
+do {
+    console.log(i);
+    i += 5;
+} while (i <= 50);  
